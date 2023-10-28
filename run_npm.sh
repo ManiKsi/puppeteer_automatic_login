0db84de40de87656548a1ad99eead2ac50c88aba
Mani <116426405+Manikanta-undru@users.noreply.github.com>
2023-10-29 02:09:48 +0530

    Update run_npm.sh

--- run_npm.sh
+++ run_npm.sh
@@ -1,9 +1,9 @@
 
 SHELL=/bin/bash
 
-# Navigate to the directory
-cd /home/mani/work/eclipton/auto-scripts
+# Navigate to the directory(Add currenct directory
+cd .......
 
 # Run node script
-/usr/bin/node /home/mani/work/eclipton/auto-scripts/index.js
+/usr/bin/node directory/auto-scripts/index.js
 
